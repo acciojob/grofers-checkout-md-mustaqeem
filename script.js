69d58dd7c08a572dd136183b435d104806afc800
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 23:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ document.body.appendChild(getSumBtn);
 const getSum = () => {
 //Add your code here
   
+  const prices = document.querySelectorAll(".prices");
+
+  let total = 0;
+
+  prices.forEach(price => {
+
+    total += Number(price.innerText);
+
+  });
+
+  const tr = document.createElement("tr");
+
+  const td = document.createElement("td");
+
+  td.colSpan = 2;
+
+  td.innerText = total;
+
+  tr.appendChild(td);
+
+  document.querySelector("table").appendChild(tr);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
-
